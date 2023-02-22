6d7beed2df4509e48bc6add40794eee5fcfc2426
julie thomas <juliethomas1008@gmail.com>
2023-02-22 16:55:30 +0100

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Vue panoramique au dernier étage au cœur de Dalston',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Superbe vue triple depuis le dernier étage, y compris une vue sur la ville (le Gherkin, la ville, etc.) et des vues supplémentaires au nord. Appartement propre, confortable, pratique et moderne pour les familles au cœur de Dalston Junction',
+  price_per_night: 97,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Villa avec jacuzzi -9 minutesToOxfordStreet-Zoo',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: "Reposez-vous devant la magnifique cheminée d'époque et détendez-vous devant la fabuleuse télévision 55. Entièrement rénové, cet appartement moderne a de nombreuses touches de luxe tout au long, du canapé en velours et tapis vintage au bain indépendant.",
+  price_per_night: 149,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: "Maisonette confortable et élégante d'Islington",
+  address: '10 Clifton Gardens London W9 1DT',
+  description: "Bienvenue chez moi à Canonbury, un superbe quartier d'Islington dans le centre de Londres. C'est une maisonnette, avec une chambre calme à l'étage, et le salon et la terrasse privée au-dessus de la rue animée Essex Road.",
+  price_per_night: 165,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 2
+)
